5f8f6e13208db5564333041fba779a44b825c0fa
tgahn04 <tgahn0524@gmail.com>
2026-01-19 19:45:36 +0900

    (2026.1.19) 경량 패턴

--- Assets/Class 2nd (Strategy Patton)/Scripts/Create_Bullet.cs
+++ Assets/Class 2nd (Strategy Patton)/Scripts/Create_Bullet.cs
@@ -3,8 +3,18 @@
 public class Create_Bullet : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
+    [SerializeField] float duration;
+    [SerializeField] Vector3 direction;
+
+    void Start()
+    {
+        Destroy(gameObject, duration);
+    }
+
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(direction * speed * Time.deltaTime);
+
+
     }
 }
--- Assets/Class 2nd (Strategy Patton)/Scripts/Grenade.cs
+++ Assets/Class 2nd (Strategy Patton)/Scripts/Grenade.cs
@@ -1,9 +1,43 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public  class Grenade : Weapon
 {
+    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] private Vector3 throwForce;
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+
+    protected override void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+
+        rigidbody.isKinematic = true;
+    }
+     
     public override void Attack()
     {
-        Debug.Log("Grenade");
+        rigidbody.isKinematic = false;
+
+        rigidbody.AddForce(throwForce, ForceMode.Impulse);
+    }
+
+    private void OnBecameInvisible()
+    {
+        ResetGrenade();
+    }
+
+    private void ResetGrenade()
+    {
+        rigidbody.linearVelocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+        rigidbody.isKinematic = true;
+
+        transform.position = startPosition;
+        transform.rotation = startRotation;
     }
 }
