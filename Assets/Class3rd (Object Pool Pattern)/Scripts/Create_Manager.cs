09663c37708513f6b5b8e0b8fd90b8d244d47ab6
tgahn04 <tgahn0524@gmail.com>
2026-01-20 19:46:15 +0900

    (2025.1.20) 어댑터 파편

---
+++ Assets/Class3rd (Object Pool Pattern)/Scripts/Create_Manager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Create_Manager : MonoBehaviour
+{
+    [SerializeField] GameObject Minotaur;
+    [SerializeField] float SpawnInterval;
+    [SerializeField] float SpawnPoint;
+    [SerializeField] Vector3 SpawnOffset;
+
+    [SerializeField] Transform Portal;
+
+    void Awake()
+    {
+        GameObject PortalObject = GameObject.Find("Portal");
+
+        if(PortalObject != null)
+        {
+            Portal = PortalObject.transform;
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            SpawnMinotaur();
+            yield return new WaitForSeconds(SpawnInterval);
+        }
+    }
+
+    void SpawnMinotaur()
+    {
+        if (Minotaur == null || Portal == null)
+            return;
+
+        Vector2 direction = Random.insideUnitCircle.normalized;
+
+        Vector3 spawnPosition = Portal.position + 
+        new Vector3(direction.x, 0f, direction.y) * SpawnPoint + SpawnOffset;
+
+        Vector3 lookDir = Portal.position - spawnPosition;
+        lookDir.y = 0f;
+
+        Quaternion rotation = Quaternion.LookRotation(lookDir);
+
+        Instantiate(Minotaur, spawnPosition, rotation);
+    }
+}
---
+++ Assets/Class3rd (Object Pool Pattern)/Scripts/Move.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Transform portal;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float arriveDistance;
+
+    void Start()
+    {
+        GameObject PortalObject = GameObject.Find("Portal");
+
+        if (PortalObject != null)
+        {
+            portal = PortalObject.transform;
+        }
+    }
+
+    void Update()
+    {
+        if (portal == null)
+            return;
+
+        Vector3 direction = portal.position - transform.position;
+        direction.y = 0f;
+
+        transform.position += direction.normalized * moveSpeed * Time.deltaTime;
+
+        if (direction.magnitude <= arriveDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
