d9e3134141101a0b1c2ab0924394b0a453527036
tgahn04 <tgahn0524@gmail.com>
2026-01-30 19:48:05 +0900

    (2026.1.30) 경량패턴

---
+++ Assets/Class6th(Fltweight Pattern)/Scripts/CorutineCache.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CorutineCache
+{
+    private static Dictionary<float, WaitForSeconds> dictionary = new Dictionary<float, WaitForSeconds>();
+   
+    public static WaitForSeconds GetCachedWait(float time)
+    {
+        if (dictionary.ContainsKey(time))
+        {
+            return dictionary[time];
+        }
+
+        else
+        {
+            WaitForSeconds waitForSeconds = null;
+
+            if(dictionary.TryGetValue(time, out waitForSeconds) == false)
+            {
+                dictionary.Add(time, new WaitForSeconds(time));
+
+                waitForSeconds = dictionary[time];
+            }
+
+            return waitForSeconds;
+        }
+    }
+
+}
---
+++ Assets/Class6th(Fltweight Pattern)/Scripts/Minotaur.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minotaur : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] AnimatorStateInfo animatorStateInfo;
+
+    private Coroutine coroutine;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+        animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+    }
+
+    private void Start()
+    {
+        coroutine = StartCoroutine(Paranoia());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(coroutine != null)
+            {
+                StopCoroutine(coroutine);
+            }
+
+            coroutine = StartCoroutine(Paranoia());
+        }
+    }
+    private IEnumerator Smash()
+    {
+        animator.SetTrigger("Smash");
+
+        AnimatorClipInfo[] clipInfos = animator.GetCurrentAnimatorClipInfo(0);
+
+        yield return CorutineCache.GetCachedWait(AnimatorClipInfo[0].clip.Length);
+    }
+
+    private IEnumerator Damnation() 
+    {
+        animator.SetTrigger("Damnation");
+
+        AnimatorClipInfo[] clipInfos = animator.GetCurrentAnimatorClipInfo(0);
+
+        yield return CorutineCache.GetCachedWait(AnimatorClipInfo[0].clip.Length);
+    }
+    
+    private IEnumerator Paranoia()
+    {
+        animator.SetTrigger("Paranoia");
+
+        
+
+        AnimatorClipInfo[] clipInfos = animator.GetCurrentAnimatorClipInfo(0);
+
+        yield return CorutineCache.GetCachedWait(AnimatorClipInfo[0].clip.Length);
+    }
+}
